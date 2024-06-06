ab43173ce45168cefc32dba5e304b3fa0a262c52
codevegitable <1057852998@qq.com>
2024-06-06 17:50:16 +0800

    new

--- LogicAndComputerFundermental/final_project/main/top.v
+++ LogicAndComputerFundermental/final_project/main/top.v
@@ -1,73 +1,103 @@
-//将屏幕80≤x＜560，0≤y＜480的部分划分成12×12的区域，只需要用144位的数组就能储存所有区域方格分布
-//数组按《从低位到高位》的顺序分别代表屏幕从左到右，从上到下的区域0-143
+//蟆�ｱ丞ｹ?80竕､x�?560�?0竕､y�?480逧�Κ蛻��蛻��12ﾃ12逧�玄蝓滂ｼ悟宵髴隕∫畑144菴咲噪謨ｰ扈�ｰｱ閭ｽ蛯ｨ蟄俶?譛牙玄蝓滓婿譬ｼ蛻�ｸ?
+//謨ｰ扈�潔?贋ｻ惹ｽ惹ｽ榊芦鬮倅ｽ阪?狗噪鬘ｺ蠎丞�蛻ｫ莉｣陦ｨ螻丞ｹ穂ｻ主ｷｦ蛻ｰ蜿ｳ�御ｻ惹ｸ雁芦荳狗噪蛹ｺ蝓0-143
 module top(
-    input clk,  //100MHz的时钟
-    input [7:0] SW, //开关
+    input clk,  //100MHz逧�慮髓?
+    input [7:0] SW, //蠑?蜈?
     input [3:0] btn,
     output btn_x,
-    output vga_hs, vga_vs,  //vga接口信号位置（不用管）
-    output [3:0] vga_red, vga_green, vga_blue   //vga的RGB信号（不用管）
-//    output buzzer     //蜂鸣器（暂无）
+    output vga_hs, vga_vs,  //vga謗･蜿｣菫｡蜿ｷ菴咲ｽｮ�井ｸ咲畑邂｡�?
+    output [3:0] vga_red, vga_green, vga_blue   //vga逧СGB菫｡蜿ｷ�井ｸ咲畑邂｡�?
+//    output buzzer     //陷るｸ｣蝎ｨ�域嘯譌�?
 );
 
-//时钟分频 获得不同频率时钟信号
+//譌ｶ髓溷�鬚 闔ｷ蠕嶺ｸ榊酔鬚醍紫譌ｶ髓滉ｿ｡蜿ｷ
 reg [31:0] clk_div;
 initial clk_div=0;
 always @(posedge clk) begin
     clk_div <= clk_div + 1'b1;
 end
 
-
-reg [143:0] occupy;     //已经下落的方块分布信息
-reg [143:0] position = 0;   //屏幕上正在下落的方块分布信息（其实只有一坨方块，有点浪费空间
-//初始化
+reg [143:0] occupy;     //蟾ｲ扈丈ｸ玖誠逧�婿蝮怜�蟶�ｿ｡諱?
+reg [143:0] position;   //螻丞ｹ穂ｸ頑ｭ｣蝨ｨ荳玖誠逧�婿蝮怜�蟶�ｿ｡諱ｯ�亥�螳槫宵譛我ｸ蝮ｨ譁ｹ蝮暦ｼ梧怏轤ｹ豬ｪ雍ｹ遨ｺ髣ｴ
+//蛻晏ｧ句?
 wire [11:0] shape;
 wire button_begin;
-game_begin begingame1(.clk(clk), .SW(SW[0]), .shape(shape));
+wire [15:0] random_option;
+//random rand(.clk(clk), .ran_seed(clk_div), .rst(1'b0), .begin_button(SW[0]), .out(random_option));
+create_block create1(.S(clk_div[28:26]), .EN(SW[0]), .shape(shape));
+wire [143:0] nextblock, nextblock_tleft, nextblock_tright, nextblock_tover;
+assign nextblock[6:4]=shape[2:0];
+assign nextblock[18:16]=shape[5:3];
+assign nextblock[30:28]=shape[8:6];
+assign nextblock[42:40]=shape[11:9]; //逕滓�譁ｹ蝮
+assign nextblock_tleft[7:4]={shape[11],shape[8],shape[5],shape[2]};
+assign nextblock_tleft[19:16]={shape[10],shape[7],shape[4],shape[1]};
+assign nextblock_tleft[31:28]={shape[9],shape[6],shape[3],shape[0]};
+assign nextblock_tright[7:4]={shape[0],shape[3],shape[6],shape[9]};
+assign nextblock_tright[19:16]={shape[1],shape[4],shape[7],shape[10]};
+assign nextblock_tright[31:28]={shape[2],shape[5],shape[8],shape[11]};
+assign nextblock_tover[6:4]={shape[9],shape[10],shape[11]};
+assign nextblock_tover[18:16]={shape[6],shape[7],shape[8]};
+assign nextblock_tover[30:28]={shape[3],shape[4],shape[5]};
+assign nextblock_tover[42:40]={shape[0],shape[1],shape[2]};
+reg [143:0] image, image_tleft, image_tright, image_tover;
+initial begin
+    image=nextblock; image_tleft=nextblock_tleft;
+    image_tright=nextblock_tright; image_tover=nextblock_tover;
+end
+wire [143:0] img_all;
+wire img_isleft, img_isright;
+assign img_all=image | image_tleft | image_tright | image_tover;
+assign img_isleft=img_all[0]|img_all[12]|img_all[24]|img_all[36]|img_all[48]|img_all[60]|img_all[72]|img_all[84]|img_all[96]|img_all[108]|img_all[120]|img_all[132];
+assign img_isright=img_all[11]|img_all[23]|img_all[35]|img_all[47]|img_all[59]|img_all[71]|img_all[83]|img_all[95]|img_all[107]|img_all[119]|img_all[131]|img_all[143];
 
 initial begin   
     occupy=0;
-    position[6:4]=shape[2:0];
-    position[18:16]=shape[5:3];
-    position[30:28]=shape[8:6];
-    position[42:40]=shape[11:9]; //生成方块
+    position={{128{1'b0}},{1'b1},{11{1'b0}},{1'b1},{3{1'b0}}};
 end
 
-//block_clk：方块下落的每帧时长
+//block_clk�壽婿蝮嶺ｸ玖誠逧�ｯ丞ｸｧ譌ｶ髟ｿ
 wire [25:0] block_clk;
-wire game_clk;
 assign block_clk = clk_div[25:0];
-assign game_clk=clk_div[23];
 
-//下落方块的下一帧位置
+//荳玖誠譁ｹ蝮礼噪荳倶ｸ?蟶ｧ菴咲ｽ?
 wire [143:0] position_below, position_left, position_right;
 assign position_below = position<<12;
 assign position_left=position>>1;
 assign position_right=position<<1;
 
-//平移、旋转按键的去抖动
+//蟷ｳ遘ｻ縲∵雷霓ｬ謖蛾醗逧�悉謚門?
 wire left, right, turn_left, turn_right;
-wire [143:0]rotate_position;
 pbdebounce debounce0(.clk(clk), .button(btn[0]), .pbreg(turn_right));
 pbdebounce debounce1(.clk(clk), .button(btn[1]), .pbreg(right));
 pbdebounce debounce2(.clk(clk), .button(btn[2]), .pbreg(left));
 pbdebounce debounce3(.clk(clk), .button(btn[3]), .pbreg(turn_left));
 assign btn_x=0;
-//到左右边界的判断（使要出界时按键失效）
+//蛻ｰ蟾ｦ蜿ｳ霎ｹ逡檎噪蛻､譁ｭ�井ｽｿ隕∝�逡梧慮謖蛾醗螟ｱ謨茨ｼ?
 wire isleftb, isrightb;
 assign isleftb=position[0]|position[12]|position[24]|position[36]|position[48]|position[60]|position[72]|position[84]|position[96]|position[108]|position[120]|position[132];
 assign isrightb=position[11]|position[23]|position[35]|position[47]|position[59]|position[71]|position[83]|position[95]|position[107]|position[119]|position[131]|position[143];
 
-//逻辑  ##不能让程序进入同一个时钟的if语句，会出现多重驱动的问题
+reg lose;
+initial lose=0;
+
+//騾ｻ霎  ##荳崎�隶ｩ遞句ｺ剰ｿ帛�蜷御ｸ?荳ｪ譌ｶ髓溽噪if隸ｭ蜿･�御ｼ壼�邇ｰ螟夐㍾鬩ｱ蜉ｨ逧�琉鬚?
 always @(posedge clk) begin
-    if(block_clk==0) begin      //最慢的时钟控制自动下落
-        if(position_below & occupy || position[143:132]) begin
+    if(block_clk[25:0]==0) begin      //譛?諷｢逧�慮髓滓而蛻ｶ閾ｪ蜉ｨ荳玖誠
+        if(position & occupy) begin
+            lose<=1;
+        end
+        else if(position_below & occupy || position[143:132] || position==0) begin
             occupy<=occupy|position;
-            position<={{128{1'b0}},1'b1,{12{1'b0}},1'b1,{2{1'b0}}};
+            position<=nextblock;
+            image<=nextblock; image_tleft<=nextblock_tleft;
+            image_tright<=nextblock_tright; image_tover<=nextblock_tover;
         end else begin
             position<=position_below;
-        end
-    end else if(block_clk[24:0]==1) begin   //次慢的时钟控制消除
+            image<=image<<12; image_tleft<=image_tleft<<12;
+            image_tright<=image_tright<<12; image_tover<=image_tover<<12;
+        end 
+    end else if(block_clk[23:0]==1) begin   //谺｡諷｢逧�慮髓滓而蛻ｶ豸磯?
         if(occupy[11:0]==12'hfff) begin
             occupy[11:0]<=0;
         end
@@ -115,12 +145,28 @@ always @(posedge clk) begin
             occupy[143:12]<=occupy[131:0];
             occupy[11:0]<=0;
         end
-    end else if(block_clk[22:0]==0) begin   //最快的时钟控制左右平移（以及旋转待完善）
+    end else if(block_clk[23:0]==0) begin
+        if((~turn_left|~turn_right) & ~(img_isleft & img_isright) & !(img_all & occupy)) begin
+            if(~turn_left) begin
+                position<=image_tleft;
+                image<=image_tleft; image_tleft<=image_tover;
+                image_tover<=image_tright; image_tright<=image;
+            end else if(~turn_right) begin
+                position<=image_tright;
+                image<=image_tright; image_tright<=image_tover;
+                image_tover<=image_tleft; image_tleft<=image;
+            end
+        end
+    end else if(block_clk[22:0]==0) begin   //譛?蠢ｫ逧�慮髓滓而蛻ｶ蟾ｦ蜿ｳ蟷ｳ遘ｻ�井ｻ･蜿頑雷霓ｬ蠕�ｮ悟埋�?
         if(~left & ~isleftb & !(position_left & occupy)) begin
             position<=position_left;
+            image<=image>>1; image_tleft<=image_tleft>>1;
+            image_tright<=image_tright>>1; image_tover<=image_tover>>1;
         end
         if(~right & ~isrightb & !(position_right & occupy)) begin
             position<=position_right;
+            image<=image<<1; image_tleft<=image_tleft<<1;
+            image_tright<=image_tright<<1; image_tover<=image_tover<<1;
         end
     end
 end
@@ -134,14 +180,14 @@ end
 //    end
 //end
 
-//�����ɫ
+//�ｽ�ｽ�ｽ�ｽ�ｽﾉ?
 reg [11:0] color;
-wire [9:0] x;   //当前像素点的原始横坐标
-wire [8:0] y;   //当前像素点的原始纵坐标
-wire [3:0] px;  //这一点所在12*12分区的横坐标
-wire [3:0] py;  //这一点所在12*12分区的纵坐标
-wire [7:0] p;   //将12*12分区坐标压成一行后，该分区的位置
-//具体赋值
+wire [9:0] x;   //蠖灘燕蜒冗ｴ轤ｹ逧�次蟋区ｨｪ蝮先?
+wire [8:0] y;   //蠖灘燕蜒冗ｴ轤ｹ逧�次蟋狗ｺｵ蝮先?
+wire [3:0] px;  //霑吩ｸ轤ｹ謇蝨?12*12蛻�玄逧�ｨｪ蝮先
+wire [3:0] py;  //霑吩ｸ轤ｹ謇蝨?12*12蛻�玄逧�ｺｵ蝮先
+wire [7:0] p;   //蟆?12*12蛻�玄蝮先�視謌蝉ｸ?陦悟錘�瑚ｯ･蛻�玄逧�ｽ咲ｽ?
+//蜈ｷ菴楢ｵ句??
 assign px=(x-80)/40;
 assign py=y/40;
 assign p=py*12+px;
@@ -149,22 +195,25 @@ assign p=py*12+px;
 
 always @(posedge clk)begin
     if(x>=80 && x<560) begin
-        if(position[p]==1||occupy[p]==1)begin   //被方块占据
-            color<=shape; //不同方块颜色不同
+        if(position[p]==1||occupy[p]==1)begin   //陲ｫ譁ｹ蝮怜頃謐?
+            color<=12'hfff; //髱樣ｻ
         end else begin
-            color<=12'h000; //即白
+            color<=12'h000; //蜊ｳ逋ｽ
         end
-    end else begin
+    end else if(nextblock!=0) begin
         color<=12'h93a;
+    end else begin
+        color<=12'h138;
     end
 end
 
-//把上面得到的这些信号输进文档里写好的vga驱动模块
+//謚贋ｸ企擇蠕怜芦逧�ｿ吩ｺ帑ｿ｡蜿ｷ霎楢ｿ帶枚譯｣驥悟�螂ｽ逧ёga鬩ｱ蜉ｨ讓｡蝮
 vgac v0(
     .vga_clk(clk_div[1]), .clrn(1'b1), .d_in(color), 
     .r(vga_red), .g(vga_green), .b(vga_blue), .hs(vga_hs), .vs(vga_vs), .col_addr(x), .row_addr(y)
 );
 
+//霑吩ｺ帶弍陷るｸ｣蝎ｨ蜿租o髻?3s逧�ｻ｣遐?
 //reg [31:0] counter;
 //initial counter=1;
 //always @(posedge clk) begin
@@ -182,12 +231,5 @@ vgac v0(
 //    .clk(clk), .note(note), .beep(buzzer)
 //);
 
-//发出7个音高
-wire note;
-create_note note1(.clk(clk), .note(note));
-
-buzzer_driver buzzer1(
-    .clk(clk), .note(note), .begin_button(SW[0]), .beep(buzzer)
-);
 
 endmodule
